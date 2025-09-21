af1a826b7a42838d89bfb158396a37850a31da83
michelleduong03 <miichelleduong@gmail.com>
2025-09-21 12:34:47 -0700

    problem 10 done

--- TIP-102/StringsArrays/session2.py
+++ TIP-102/StringsArrays/session2.py
@@ -188,6 +188,24 @@ merge_alternately(word1, word2)
 
 # problem 10
 
+def good_pairs(pile1, pile2, k):
+    count = 0
+    for i in range(len(pile1)):
+        for j in range(len(pile2)):
+            if pile2[j] * k != 0 and pile1[i] % (pile2[j] * k) == 0:
+                count += 1
+    print(count)
+
+pile1 = [1, 3, 4]
+pile2 = [1, 3, 4]
+k = 1
+good_pairs(pile1, pile2, k)
+
+pile1 = [1, 2, 4, 12]
+pile2 = [2, 4]
+k = 3
+good_pairs(pile1, pile2, k)
+
 # problem 11
 
 # problem 12
