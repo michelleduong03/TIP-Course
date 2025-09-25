432d565c913d0e2f94997edc56363787d5977d30
michelleduong03 <miichelleduong@gmail.com>
2025-09-25 12:24:02 -0700

    problem 10

--- TIP-102/Dictionaries/session3.py
+++ TIP-102/Dictionaries/session3.py
@@ -162,6 +162,8 @@ def find_stage_arrangement_difference(s, t):
     :rtype: int
     """
     index_map = {performer: i for i, performer in enumerate(s)}
+    # performer is key and i is index. so we are enumerating through
+    # s with a key value stored in the index_map
     
     total_diff = 0
     for j, performer in enumerate(t):
@@ -176,3 +178,35 @@ t2 = ["Eve", "David", "Bob", "Alice", "Charlie"]
 
 print(find_stage_arrangement_difference(s1, t1))
 print(find_stage_arrangement_difference(s2, t2))
+
+# problem 10
+
+# 1. Create an empty set called vip_set.
+# 2. For each character in vip_passes, add it to vip_set.
+# 3. Initialize a counter variable to 0.
+# 4. For each character in guests:
+#    * If the character is in vip_set, increment the count by 1.
+# 5. Return the count.
+
+def num_VIP_guests(vip_passes, guests):
+    vip_set = set()
+
+    for i in vip_passes:
+        vip_set.add(i)
+    
+    count = 0
+
+    for i in guests:
+        if i in vip_set:
+            count += 1
+    
+    return count
+
+vip_passes1 = "aA"
+guests1 = "aAAbbbb"
+
+vip_passes2 = "z"
+guests2 = "ZZ"
+
+print(num_VIP_guests(vip_passes1, guests1))
+print(num_VIP_guests(vip_passes2, guests2))
