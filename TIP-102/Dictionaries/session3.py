f9c33537e7fb48712fead9eb13dc3ce046fb9a62
michelleduong03 <miichelleduong@gmail.com>
2025-09-25 12:27:06 -0700

    problem 11

--- TIP-102/Dictionaries/session3.py
+++ TIP-102/Dictionaries/session3.py
@@ -210,3 +210,75 @@ guests2 = "ZZ"
 
 print(num_VIP_guests(vip_passes1, guests1))
 print(num_VIP_guests(vip_passes2, guests2))
+
+# problem 11
+
+# You are provided with a partially implemented and buggy version of the solution. 
+# Identify and fix the bugs in the code. Then, perform a thorough code review and 
+# suggest improvements.
+
+# def schedule_pattern(pattern, schedule):
+    
+#     genres = schedule.split()
+
+#     if len(genres) == len(pattern):
+#         return True
+
+#     char_to_genre = {}
+#     genre_to_char = {}
+
+#     for char, genre in zip(pattern, genres):
+#         if char in char_to_genre:
+#             if char_to_genre[char] == genre:
+#                 return True
+#         else:
+#             char_to_genre[char] = genre
+
+#         if genre in genre_to_char:
+#             if genre_to_char[genre] == char:
+#                 return True
+#         else:
+#             genre_to_char[genre] = char
+
+#     return False
+
+def schedule_pattern(pattern, schedule):
+    genres = schedule.split()
+
+    # Fix 1: If lengths don't match, fail early
+    if len(genres) != len(pattern):
+        return False
+
+    char_to_genre = {}
+    genre_to_char = {}
+
+    for char, genre in zip(pattern, genres):
+        # Check if pattern char already mapped to a genre
+        if char in char_to_genre:
+            if char_to_genre[char] != genre:
+                return False
+        else:
+            char_to_genre[char] = genre
+
+        # Check if genre already mapped to a pattern char
+        if genre in genre_to_char:
+            if genre_to_char[genre] != char:
+                return False
+        else:
+            genre_to_char[genre] = char
+
+    # If we finish loop without contradiction, it's a valid pattern
+    return True
+
+pattern1 = "abba"
+schedule1 = "rock jazz jazz rock"
+
+pattern2 = "abba"
+schedule2 = "rock jazz jazz blues"
+
+pattern3 = "aaaa"
+schedule3 = "rock jazz jazz rock"
+
+print(schedule_pattern(pattern1, schedule1))
+print(schedule_pattern(pattern2, schedule2))
+print(schedule_pattern(pattern3, schedule3))
