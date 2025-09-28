1f725586564ef0886035118c7586aa6b3c726322
michelleduong03 <miichelleduong@gmail.com>
2025-09-27 19:07:17 -0700

    inclass problems

---
+++ TIP-102/Dictionaries/session4.py
@@ -0,0 +1,115 @@
+# IC
+
+def find_difference(signals1, signals2):
+    set1 = set(signals1)
+    set2 = set(signals2)
+    
+    diff1 = list(set1.difference(set2))
+    diff2 = list(set2.difference(set1))
+    
+    return [diff1, diff2]
+
+s1 = [1, 2, 3, 4]
+s2 = [2, 3, 5, 9]
+
+print(find_difference(s1, s2))
+
+
+
+# problem 1
+
+def most_endangered(species_list):
+
+    if not species_list:
+        return None
+    
+    most_endangered_species = species_list[0]
+    
+    for species in species_list[1:]:
+        if species["population"] < most_endangered_species["population"]:
+            most_endangered_species = species
+    
+    return most_endangered_species["name"]
+
+    # if not species_list:
+    #     return None
+    
+    # return min(species_list, key=lambda species: species["population"])
+
+species_list = [
+    {"name": "Amur Leopard",
+     "habitat": "Temperate forests",
+     "population": 84
+    },
+    {"name": "Javan Rhino",
+     "habitat": "Tropical forests",
+     "population": 72
+    },
+    {"name": "Vaquita",
+     "habitat": "Marine",
+     "population": 10
+    }
+]
+
+print(most_endangered(species_list))
+
+# problem 2
+
+def count_endangered_species(endangered_species, observed_species):
+    endangered_set = set(endangered_species)
+    
+    count = 0
+    for i in observed_species:
+        if i in endangered_set:
+            count += 1
+    
+    return count
+
+
+endangered_species1 = "aA"
+observed_species1 = "aAAbbbb"
+
+endangered_species2 = "z"
+observed_species2 = "ZZ"
+
+print(count_endangered_species(endangered_species1, observed_species1)) 
+print(count_endangered_species(endangered_species2, observed_species2))  
+
+# problem 3
+
+def navigate_research_station(station_layout, observations):
+    index_map = {char: i for i, char in enumerate(station_layout)}
+    
+    total_time = 0
+    current_pos = 0  
+    
+    for obs in observations:
+        next_pos = index_map[obs]
+        total_time += abs(current_pos - next_pos)
+        current_pos = next_pos
+    
+    return total_time
+
+
+station_layout1 = "pqrstuvwxyzabcdefghijklmno"
+observations1 = "wildlife"
+
+station_layout2 = "abcdefghijklmnopqrstuvwxyz"
+observations2 = "cba"
+
+print(navigate_research_station(station_layout1, observations1))  
+print(navigate_research_station(station_layout2, observations2))
+
+# problem 4
+
+def prioritize_observations(observed_species, priority_species):
+  pass
+
+observed_species1 = ["🐯", "🦁", "🦌", "🦁", "🐯", "🐘", "🐍", "🦑", "🐻", "🐯", "🐼"]
+priority_species1 = ["🐯", "🦌", "🐘", "🦁"]  
+
+observed_species2 = ["bluejay", "sparrow", "cardinal", "robin", "crow"]
+priority_species2 = ["cardinal", "sparrow", "bluejay"]
+
+print(prioritize_observations(observed_species1, priority_species1))
+print(prioritize_observations(observed_species2, priority_species2)) 
