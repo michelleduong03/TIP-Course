e47393323b046183f6cc6acf3a8879479e10c388
michelleduong03 <miichelleduong@gmail.com>
2025-10-07 16:38:48 -0700

    wokring on 1

---
+++ TIP-102/review.py
@@ -0,0 +1,23 @@
+# problem 1
+def extract_nft_names(nft_collection):
+    pass
+
+# Example usage:
+nft_collection = [
+    {"name": "Abstract Horizon", "creator": "ArtByAlex", "value": 5.4},
+    {"name": "Pixel Dreams", "creator": "DreamyPixel", "value": 7.2},
+    {"name": "Future City", "creator": "UrbanArt", "value": 3.8}
+]
+
+nft_collection_2 = [
+    {"name": "Crypto Kitty", "creator": "CryptoPets", "value": 10.5},
+    {"name": "Galactic Voyage", "creator": "SpaceArt", "value": 6.7}
+]
+
+nft_collection_3 = [
+    {"name": "Golden Hour", "creator": "SunsetArtist", "value": 8.9}
+]
+
+print(extract_nft_names(nft_collection))
+print(extract_nft_names(nft_collection_2))
+print(extract_nft_names(nft_collection_3))
