749e5b10d3bebe7f2f2d7326a72454b4582b7c9c
michelleduong03 <miichelleduong@gmail.com>
2025-10-09 18:24:59 -0700

    rpobelm 4

--- TIP-102/review.py
+++ TIP-102/review.py
@@ -90,3 +90,31 @@ nft_collection_3 = [
 print(identify_popular_creators(nft_collection))
 print(identify_popular_creators(nft_collection_2))
 print(identify_popular_creators(nft_collection_3))
+
+# problem 4
+def average_nft_value(nft_collection):
+    if not nft_collection:  # check if empty
+        return 0
+    
+    total = 0
+    for nft in nft_collection:
+        total += nft["value"]
+    
+    return total / len(nft_collection)
+
+
+nft_collection = [
+    {"name": "Abstract Horizon", "creator": "ArtByAlex", "value": 5.4},
+    {"name": "Pixel Dreams", "creator": "DreamyPixel", "value": 7.2},
+    {"name": "Urban Jungle", "creator": "ArtByAlex", "value": 4.5}
+]
+print(average_nft_value(nft_collection))
+
+nft_collection_2 = [
+    {"name": "Golden Hour", "creator": "SunsetArtist", "value": 8.9},
+    {"name": "Sunset Serenade", "creator": "SunsetArtist", "value": 9.4}
+]
+print(average_nft_value(nft_collection_2))
+
+nft_collection_3 = []
+print(average_nft_value(nft_collection_3))
