7d87b4cceb70a9eaa46fde4b97e37b7aa82131c6
michelleduong03 <miichelleduong@gmail.com>
2025-10-15 12:02:33 -0700

    problems 4-5 done

--- TIP-102/LinkedLists1/session1.py
+++ TIP-102/LinkedLists1/session1.py
@@ -9,6 +9,21 @@ class Villager:
     # problem 2
     def greet_player(self, player_name):
         return f"{self.name}: Hey there, {player_name}! How's it going, {self.catchphrase}!"
+    
+    # problem 4
+    def set_catchphrase(self, new_catchphrase):
+        # Check that the catchphrase is alphabetic or whitespace and less than 20 characters
+        if len(new_catchphrase) < 20 and all(c.isalpha() or c.isspace() for c in new_catchphrase):
+            self.catchphrase = new_catchphrase
+            print("Catchphrase updated!")
+        else:
+            print("Invalid catchphrase")
+
+    # Problem 5: Add Furniture
+    def add_item(self, item_name):
+        valid_items = ["acoustic guitar", "ironwood kitchenette", "rattan armchair", "kotatsu", "cacao tree"]
+        if item_name in valid_items:
+            self.furniture.append(item_name)
 
 apollo = Villager("Apollo", "Eagle", "pah")
 
@@ -25,3 +40,4 @@ print(bones.greet_player("Michelle"))
 
 print("problem 3")
 bones.catchphrase = "ruff it up"
+
