32250ab5e11719de7c0fd626d1b49f1c1db04b8d
michelleduong03 <miichelleduong@gmail.com>
2025-10-14 18:07:45 -0700

    problem 3

--- TIP-102/LinkedLists1/session1.py
+++ TIP-102/LinkedLists1/session1.py
@@ -6,10 +6,22 @@ class Villager:
         self.catchphrase = catchphrase
         self.furniture = []
 
+    # problem 2
+    def greet_player(self, player_name):
+        return f"{self.name}: Hey there, {player_name}! How's it going, {self.catchphrase}!"
+
 apollo = Villager("Apollo", "Eagle", "pah")
 
+print("problem 1")
 print(apollo.name)  
 print(apollo.species)  
 print(apollo.catchphrase) 
 print(apollo.furniture) 
 
+
+bones = Villager("Bones", "Dog", "yip yip")
+print("problem 2")
+print(bones.greet_player("Michelle"))
+
+print("problem 3")
+bones.catchphrase = "ruff it up"
