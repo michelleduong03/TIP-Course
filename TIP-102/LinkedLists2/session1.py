b949b99be0a45066142d8d6389f35dfb380a2617
michelleduong03 <miichelleduong@gmail.com>
2025-10-22 19:37:15 -0700

    problem 4

--- TIP-102/LinkedLists2/session1.py
+++ TIP-102/LinkedLists2/session1.py
@@ -84,5 +84,36 @@ playlist = SongNode("SOS", "ABBA",
 print("---PROBLEM 3---")
 print_linked_list(remove_song(playlist, "Dreams"))
 
+# PROBLEM 4
+class SongNode:
+    def __init__(self, song, artist, next=None):
+        self.song = song
+        self.artist = artist
+        self.next = next
+
+def on_repeat(playlist_head):
+    slow = playlist_head
+    fast = playlist_head
+
+    while fast and fast.next:
+        slow = slow.next    
+        fast = fast.next.next 
+        if slow == fast: 
+            return True
+
+    return False     
+
 print("---PROBLEM 4---")
+song1 = SongNode("GO!", "Common")
+song2 = SongNode("N95", "Kendrick Lamar")
+song3 = SongNode("WIN", "Jay Rock")
+song4 = SongNode("ATM", "J. Cole")
+song1.next = song2
+song2.next = song3
+song3.next = song4
+song4.next = song2  # cycle back to song2
+
+print(on_repeat(song1))  # True
+
+
 print("---PROBLEM 5---")
