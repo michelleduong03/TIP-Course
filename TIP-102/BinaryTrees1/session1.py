9ea20b62e1ce705abd00030ad4a8b76b6c90a5ae
michelleduong03 <miichelleduong@gmail.com>
2025-11-05 14:53:44 -0800

    problem 6

--- TIP-102/BinaryTrees1/session1.py
+++ TIP-102/BinaryTrees1/session1.py
@@ -285,3 +285,55 @@ print(count_leaves(oak2))
 # Example Output:
 # 3
 # 1
+
+
+
+# Problem 6: Pruning Plans
+
+"""
+You have a large overgrown Magnolia tree that's in desperate need of 
+some pruning. Before you can prune the tree, you need to do a full survey of 
+the tree to evaluate which sections need to be pruned.
+
+Given the root of a binary tree representing the magnolia, return a list 
+of the values of each node using a postorder traversal. In a postorder traversal, 
+you explore the left subtree first, then the right subtree, and finally the root. 
+Postorder traversals are often used when deleting nodes from a tree.
+
+Evaluate the time complexity of your function. Define your variables and 
+provide a rationale for why you believe your solution has the stated time 
+complexity. Assume the input tree is balanced when calculating time complexity.
+"""
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+def survey_tree(root):
+    if not root:
+        return []
+    
+    left = survey_tree(root.left)
+    right = survey_tree(root.right)
+    
+    return left + right + [root.val]
+
+# Example Usage:
+
+r"""
+        Root
+      /      \
+    Node1    Node2
+  /         /    \
+Leaf1    Leaf2  Leaf3
+"""
+
+magnolia = TreeNode("Root", 
+                TreeNode("Node1", TreeNode("Leaf1")),
+                TreeNode("Node2", TreeNode("Leaf2"), TreeNode("Leaf3")))
+
+print(survey_tree(magnolia))
+
+# Example Output:
+# ["Leaf1", "Node1", "Leaf2", "Leaf3", "Node2", "Root"]
