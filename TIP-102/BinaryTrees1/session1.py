7a681444592cc88f38703452224c790bf4ae670b
michelleduong03 <miichelleduong@gmail.com>
2025-11-04 18:32:45 -0800

    problem 5

--- TIP-102/BinaryTrees1/session1.py
+++ TIP-102/BinaryTrees1/session1.py
@@ -228,3 +228,60 @@ print(right_vine(ivy2))
 
 ['Root', 'Node2', 'Leaf3']
 ['Root']
+
+
+
+# Problem 5: Count the Tree Leaves
+
+"""
+You've grown an oak tree from a tiny little acorn and it's finally sprouting leaves! 
+Given the root of a binary tree representing your oak tree, count the number of leaf 
+nodes in the tree. A leaf node is a node that does not have any children.
+
+Evaluate the time complexity of your function. Define your variables 
+and provide a rationale for why you believe your solution has the stated time complexity. 
+Assume the input tree is balanced when calculating time complexity.
+"""
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+def count_leaves(root):
+    if not root:
+        return 0
+    if not root.left and not root.right:
+        return 1
+    return count_leaves(root.left) + count_leaves(root.right)
+
+# Example Usage:
+
+r"""
+        Root
+      /      \
+    Node1    Node2
+  /         /    \
+Leaf1    Leaf2  Leaf3
+"""
+
+oak1 = TreeNode("Root", 
+                TreeNode("Node1", TreeNode("Leaf1")),
+                TreeNode("Node2", TreeNode("Leaf2"), TreeNode("Leaf3")))
+
+r"""
+      Root
+      /  
+    Node1
+    /
+  Leaf1  
+"""
+oak2 = TreeNode("Root", TreeNode("Node1", TreeNode("Leaf1")))
+
+
+print(count_leaves(oak1))
+print(count_leaves(oak2))
+
+# Example Output:
+# 3
+# 1
